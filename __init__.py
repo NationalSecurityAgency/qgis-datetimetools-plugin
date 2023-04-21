643e32611eb99aa85229106244de81664776964c
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-04-21 16:28:40 +0000

    No long ship libraries with plugin

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-import os
-import site
-site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/libs'))
-
 def classFactory(iface):
     from .datetimetools import DateTimeTools
     return DateTimeTools(iface)
