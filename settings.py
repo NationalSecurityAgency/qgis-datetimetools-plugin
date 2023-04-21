e8f709519930b8422e6c26afb3118c58060824c0
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-04-21 16:30:33 +0000

    Change import

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 import os
 from qgis.core import QgsCoordinateReferenceSystem
-from timezonefinder import TimezoneFinder
+from timezonefinder.timezonefinder import TimezoneFinder
 
 
 class InitTimeZoneFinder():
     tzf = None
     def getTZF(self):
         if not self.tzf:
-            self.tzf = TimezoneFinder(bin_file_location=os.path.join(os.path.dirname(__file__), 'libs/timezonefinder'))
+            self.tzf = TimezoneFinder()
         return( self.tzf )
         
 tzf_instance = InitTimeZoneFinder()
